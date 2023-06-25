d9fae2ee807965fd2ab87d6666d4e2074ad7b39f
Dakota Ponder <dponder12358@gmail.com>
2023-06-25 14:58:17 -0500

    imported particles.js and the nasa logo to the project

--- js/main.js
+++ js/main.js
@@ -1,18 +1,27 @@
-//Example fetch using pokemonapi.co
+
+
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
+
+  // check if data for the date exists 
+  let cachedData = localStorage.getItem(document.querySelector('input').value)
+ 
+  // if data does not exist, fetch it from the API
+  if (cachedData) {
+    let data = JSON.parse(cachedData)
+    displayData(data)
+  }
+
+
+
   const choice = document.querySelector('input').value
   const url = `https://api.nasa.gov/planetary/apod?api_key=74xyntFXrm1TO9twr9n0U8nY7NIxxuCedo363Lmt&date=${choice}`
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        if (data.media_type === "image") {
-          document.querySelector('img').src = data.hdurl
-        } else if (data.media_type === "video") {
-          document.querySelector('iframe').src = data.url
-        }
+        displayData(data)
         
         // show the explanation
         document.querySelector('h3').innerText = data.explanation
@@ -25,3 +34,28 @@ function getFetch(){
           console.log(`error ${err}`)
       });
 }
+
+// function to display the data 
+function displayData(data) {
+   if (data.media_type === "image") {
+          document.querySelector('#apod-img').src = data.hdurl
+        } else if (data.media_type === "video") {
+          document.querySelector('iframe').src = data.url
+        }
+}
+
+// particles.js function for the background
+window.onload = function() {
+  Particles.init({
+    selector: '.background',
+    color: '#ECEFF1',
+    //connectParticles: true,
+    maxParticles: 150,
+    sizeVariations: 3,
+    
+    
+
+
+    
+  });
+}; 
